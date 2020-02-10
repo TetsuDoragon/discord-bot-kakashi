247d4b989dc6388aef01f3586cc789f574b33647
João Vítor S. F <48962164+jjoaovitor7@users.noreply.github.com>
2020-02-09 23:36:22 -0300

    Zabuza Momochi, O Demônio do Gás Oculto.

--- WebHook.js
+++ WebHook.js
@@ -1,13 +1,12 @@
 const webhook = require("webhook-discord")
 const Hook = new webhook.Webhook("https://discordapp.com/api/webhooks/633490341651283989/javkUzUtCDzdpKk5kRVpak5RL_ZhUz17EfuS8JqZcGE9uaRZEDMtmBzYBC8koza5wMVK")
 
-
-
 exports.sendMessage = async function(user, message){
 
 	const msg = await new webhook.MessageBuilder()
 	                .setName(user.username)
 	                .setText(message)
 	                .setAvatar(user.avatarURL)
+
 	Hook.send(msg);
 }
--- index.js
+++ index.js
@@ -1,27 +1,35 @@
 const Discord = require('discord.js');
 const kakashi = new Discord.Client();
 const copy = require('./copy.js').start;
-kakashi.login('NjMyNjc5MDE2OTQ3NTE1NDQx.XaI6bw.YGXoCxOig9SiHCtbmSlTxp4WD1M');
-kakashi.on('error', console.error);
 
+kakashi.login('TOKEN');
+kakashi.on('error', console.error);
 
 kakashi.once('ready', () => {
 	console.log("Ready!");
 });
 
 kakashi.on('message', message =>{
-	if (message.author.bot)return;
+	if (message.author.bot)
+		return;
+
 	var args = message.content.slice('!').trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 
 	if(command === "!copy"){
 		message.delete()
 		copy(kakashi, message.author, args)
-	}else 
-	if(command === "!fake"){
+	}
+	else if(command === "!fake"){
 		message.delete()
 		copy(kakashi, message.mentions.users.array()[0], args)
 	}
 
+	//else if (command === "!zabuza"){
+	//	client.channel.send("Ushi, Sara, Oo, Nae, Ee, Tori, Ushi, Uma, Tori, Nae,"
+	//						+" Tora, Inu, Tora, Mee, Ushi, Hitsuji, Mee, Ee, Hitsuji, Nae,"
+	//						+" Jin, Sara, Tori, Tatsu, Tori, Ushi, Uma, Hitsuji, Tora, Mee,"
+	//						+" Nae, Sara, Oo, Ee, Tatsu, Hitsuji, Nae, Ushi, Sara, Tori,"
+	//						+" Jin, Nae, Ee, Tori.");
+	//}
 });
-
